0ba0d015d1d9e11463d04819733796b938207265
WHAT8741 <157754903+WHAT8741@users.noreply.github.com>
2024-02-08 16:37:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Sample data for the pie chart
-// Change to my results of the test - get from school
-var data = [36.99999996, 0, 30, 0];
-var colors = ['#00FF00', '#36A2EB', '#FFFF00', '#808080'];
+var data = [0, 0, 0, 100];
+var colors = ['#FF6384', '#36A2EB', '#FFFF00', '#808080'];
 
 var canvas = document.getElementById('myPieChart');
 var ctx = canvas.getContext('2d');
@@ -21,25 +19,3 @@ for (var i = 0; i < data.length; i++) {
 
   startAngle += sliceAngle;
 }
-
-
-// Draw a half circle
-// ctx.beginPath();
-// ctx.arc(200, 200, 100, 0, 2 * Math.PI, false);
-// ctx.lineTo(200, 200);
-// ctx.lineTo(300, 200);
-// ctx.fillStyle = 'blue';
-// ctx.fill();
-// ctx.moveTo(200, 300);
-// ctx.lineTo(200, 200);
-// ctx.fillStyle = 'red';
-// ctx.fill();
-// ctx.moveTo(100, 200);
-// ctx.lineTo(200, 200);
-// ctx.moveTo(200, 100);
-// ctx.lineTo(200, 200);
-// ctx.moveTo(130, 130);
-// ctx.lineTo(200, 200);
-// ctx.moveTo(270, 270);
-// ctx.lineTo(200, 200);
-// ctx.stroke();
